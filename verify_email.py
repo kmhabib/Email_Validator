f40fcb449c12ffb9e4f3b41d26ea120c5ca06fb7
Ubuntu <kmhabib@gmail.com>
2016-12-26 14:41:17 +0000

    added ability to write to file while executing

--- verify_email.py
+++ verify_email.py
@@ -20,6 +20,7 @@ def open_emails(fname):
 					pass
 				else:
 					email_list.append(', '.join(row)) 
+	print("The total # of emails in the CSV file you have uploaded is:",len(email_list))
 	return email_list
 
 def find_mx_host(records):
@@ -37,6 +38,17 @@ def connect_validate(email_list):
 
 	validated_emails = []
 	interesting_emails = []
+
+	timestr1 = time.strftime("%Y%m%d-%H%M%S")
+	fname1 = "verified_emails_" + timestr1 + ".csv"
+	#with open(os.path.join("output_files",fname1), 'wb') as csvfile1:
+	#	emailwriter1 = csv.writer(csvfile1, dialect='excel')
+
+	timestr2 = time.strftime("%Y%m%d-%H%M%S")
+	fname2 = "interesting_emails_" + timestr2 + ".csv"
+	#with open(os.path.join("output_files",fname2), 'wb') as csvfile2:
+	#	emailwriter2 = csv.writer(csvfile2, dialect='excel')
+	count = 0	
 	for email in email_list:
 		print(email)
 		match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
@@ -69,6 +81,7 @@ def connect_validate(email_list):
 			server.mail('verify@verified.com')
 			code, message = server.rcpt(str(email))
 			server.quit()
+			count = count + 1
 		except:
 			print("MX error", mxRecord)
 			continue
@@ -79,14 +92,24 @@ def connect_validate(email_list):
 		# Assume 250 as Success
 		if code == 250:
 			print('Success')
-			validated_emails.append(email)
+			with open(os.path.join("output_files",fname1), 'wb') as csvfile1:
+				emailwriter1 = csv.writer(csvfile1, dialect='excel')
+				#emailwriter1.writerow(validated_emails.append(email))
+				emailwriter1.writerow([email])
 		else:
 			#interesting_emails = numpy.array([[email,code,message]])
-			interesting_emails.append(email+" "+ str(code) + " " +message)
-	return validated_emails, interesting_emails
+			#emailwriter2.writerow(interesting_emails.append(email+" "+ str(code) + " " +message)
+			interesting_string = email+" "+str(code) + " " +message
+			#emailwriter2.writerow([email+" "+ str(code) + " " +message])
+			with open(os.path.join("output_files",fname2), 'wb') as csvfile2:
+				emailwriter2 = csv.writer(csvfile2, dialect='excel')
+				print interesting_string
+				emailwriter2.writerow([interesting_string])
+	#return validated_emails, interesting_emails
 
 #print interesting_emails
 #write to verified_emails file
+"""
 def write_verified( validated_emails):
 	timestr = time.strftime("%Y%m%d-%H%M%S")
 	fname = "verified_emails_" + timestr + ".csv"
@@ -94,7 +117,9 @@ def write_verified( validated_emails):
 		emailwriter = csv.writer(csvfile, dialect='excel')
 		for row in validated_emails:
 			emailwriter.writerow([row])
+"""
 	
+"""
 #write to interesting_emails file
 def write_interesting(interesting_emails):
 	timestr = time.strftime("%Y%m%d-%H%M%S")
@@ -103,14 +128,17 @@ def write_interesting(interesting_emails):
 		emailwriter = csv.writer(csvfile, dialect='excel')
 		for row in interesting_emails:
 			emailwriter.writerow([row])
-	
+"""	
 def main(fname1):
 	email_list = open_emails(fname1) 	
-	validated_emails,interesting_emails = connect_validate(email_list)
+	#validated_emails,interesting_emails = connect_validate(email_list)
+	count =connect_validate(email_list)
+	print("count is:", count)
+	#validated_emails,interesting_emails = connect_validate(email_list)
         #write_verified(fname2, validated_emails)
-	write_verified( validated_emails)
+	#write_verified( validated_emails)
         #write_interesting(fname3, interesting_emails)
-	write_interesting(interesting_emails)
+	#write_interesting(interesting_emails)
 
 if __name__ == '__main__':
 	main(sys.argv[1])
